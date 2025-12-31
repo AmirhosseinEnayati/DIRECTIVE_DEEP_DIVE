9a3005055c4cc2be7e6be57f189bd9e29fe6463e
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-31 22:53:44 +0330

    access host element in directive

--- src/app/directives/app-safe-link.ts
+++ src/app/directives/app-safe-link.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -9,12 +9,14 @@ import { Directive, input } from '@angular/core';
 })
 export class SafeLinkDirective {
   queryString = input('defaultValue', { alias: 'appSafeLink' });
+  private hostElement = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   onConfirmPageLeave(event: MouseEvent) {
     const confirmResult = window.confirm('Do you want to leave the app?');
 
     if (!confirmResult) event?.preventDefault();
 
-    (event.target as HTMLAnchorElement).href += '?from=' + this.queryString();
+    this.hostElement.nativeElement.href += '?from=' + this.queryString();
 
     return;
   }
